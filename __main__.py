2af9ed1ee9c6bd3eeea38e265f5f64dc0bfed470
charbel <charbel.kaed@gmailk.com>
2017-10-21 21:17:50 -0400

    update loop

---
+++ __main__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import asyncio
+from reader import market_data
+from periodic import Periodic
+
+@asyncio.coroutine
+async def main():
+    """Main Of the Agent"""
+    p = Periodic(lambda: market_data(), 1)
+    try:
+        print('Updating Market Data')
+        await p.start()
+        await asyncio.sleep(3.1)
+        #await p._run()
+
+    finally:
+        print('Finally')
+        #await p.stop()  # we should stop task finally
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+
+try:
+
+    loop.run_until_complete(main())
+    loop.run_forever()
+
+finally:
+
+    print('closing event loop')
+    loop.close()
--- reader.py
+++ reader.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 import asyncio
 import json
+import datetime
 import websockets
 
 
+@asyncio.coroutine
 async def market_data():
     """Returns the latest market data"""
     async with websockets.connect('wss://api.gemini.com/v1/marketdata/ethusd') as websocket:
         var_eth_usd_price = await websocket.recv()
         data = json.loads(var_eth_usd_price)
         var_type = data["type"]
-        print("{}".format(var_eth_usd_price))
-        print("--\n"+var_type)
-
-asyncio.get_event_loop().run_until_complete(market_data())
+       # print("{}".format(var_eth_usd_price))
+        print("====> "+var_type+ " , Retrieval Time "+str(datetime.datetime.now()))
+      
+#asyncio.get_event_loop().run_until_complete(market_data())
